47020e58441cb9df433297d48703c486865cf7d9
Roman <kurpelroma@gmail.com>
2023-08-09 15:19:40 +0300

    create and designing sections about, cases

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: 'true'
+    },
+    breakpoints: {
+      1189: {
+        slidesPerView: 3
+      }
+    }
+  });
+  
